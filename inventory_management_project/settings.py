3675949b266a87b0d0ec815bc1684ae67a1f8128
Ankit Kadam <akankitkadam626@gmail.com>
2026-01-13 20:48:21 +0530

    Update settings.py

--- inventory_management_project/settings.py
+++ inventory_management_project/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--x7!&q17jzl09qr8ew)bpq09$uq5gt@##!@4*=ecs+dqbz)yki'
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -146,4 +146,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Auth_USER_MODEL = "core_app.Customuser"
